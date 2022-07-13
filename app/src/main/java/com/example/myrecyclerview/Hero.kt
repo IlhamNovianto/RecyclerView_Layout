d5f72dfa4ad79cacf4866b5094a60a18b0beb72d
Shiranita <95646373+Shiranita@users.noreply.github.com>
2022-07-13 20:37:18 +0700

    RecyclerView

---
+++ app/src/main/java/com/example/myrecyclerview/Hero.kt
@@ -0,0 +1,39 @@
+package com.example.myrecyclerview
+
+import android.os.Parcel
+import android.os.Parcelable
+
+
+
+data class Hero(
+    var name: String,
+    var description: String,
+    var Photo: String
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString().toString(),
+        parcel.readString().toString(),
+        parcel.readString().toString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(description)
+        parcel.writeString(Photo)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Hero> {
+        override fun createFromParcel(parcel: Parcel): Hero {
+            return Hero(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Hero?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
